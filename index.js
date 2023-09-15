d4ab30e01d68b5ac4a807801412fbd63f04ccffe
Alexander kagerer <kagerer19@gmail.com>
2023-09-15 07:32:51 +0200

    Add pangram logic

---
+++ index.js
@@ -0,0 +1,12 @@
+function isPangram(string) {
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    for (let i = 0; i < alphabet.length; i++) {
+        if (strArr.indexOf(alphabet[i]) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+isPangram("The quick brown fox jumps over the lazy dog");
